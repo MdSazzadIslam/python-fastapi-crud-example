526d04b11bc36ae16b64e46de260af603fb99749
MdSazzadIslam <netsazzad@gmail.com>
2024-06-09 16:08:21 +0200

    Implemented rate limiting with a limit of 10 requests per second for all endpoints

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
+
 from src.routes.product_route import router
 
-app = FastAPI()
-origins = ["*"]
+limiter = Limiter(key_func=get_remote_address, default_limits=["10/5seconds"])
 
+app = FastAPI()
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -13,6 +29,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(SlowAPIMiddleware)
 
 app.include_router(router=router, prefix="/products")
 
