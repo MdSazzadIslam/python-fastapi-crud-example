0e5524d82b0a4d4f822896387f0d4b28da439657
MdSazzadIslam <netsazzad@gmail.com>
2024-06-09 10:15:22 +0200

    Implemented CRUD operations for product management, enhancing data handling and accessibility.

---
+++ src/crud/product_crud.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+from sqlalchemy.orm import Session
+from src.models.product import Product
+from src.schemas.product_schema import ProductSchema, UpdateProductSchema, DeleteProductResponse
+
+def get_all(db: Session):
+    return db.query(Product).all()
+
+def get_by_id(product_id: UUID, db: Session):
+    return db.query(Product).filter_by(id=product_id).one()
+
+def insert_product(product: ProductSchema, db: Session):
+    product = Product(name=product.name, description=product.description)
+    db.add(product)
+    db.commit()
+    db.refresh()
+    return product
+
+def update_product(product: UpdateProductSchema, db: Session):
+    query = {Product.name: product.name, Product.description: product.description}
+    db.query(Product).filter_by(id=product.id).update(query)
+    db.commit()
+    return db.query(Product).filter_by(id=product.id).one()
+
+
+def delete_product(product_id: UUID, db: Session):
+    product = db.query(Product).filter_by(id=product_id).all()
+    if not product:
+        return DeleteProductResponse(detail="Product does not exist")
+    db.query(Product).filter_by(id=product_id).delete()
+    db.commit()
+    return DeleteProductResponse(detail="Product deleted successfully")
+
