29ea43d3ae528bb5d51e6c6b8a93f5a8d72bb1b0
MdSazzadIslam <netsazzad@gmail.com>
2024-06-09 10:11:50 +0200

    Implemented product model for database schema

---
+++ src/models/product.py
@@ -0,0 +1,30 @@
+from sqlalchemy import String, Column, TIMESTAMP, text
+from sqlalchemy.ext.declarative import declarative_base
+from uuid import uuid4
+
+Base = declarative_base()
+
+def generate_uuid():
+    return str(uuid4())
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(
+        String(255),
+        default=uuid4,
+        primary_key=True,
+        index=True
+    )
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(String(200), nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default= text('Now()'), nullable=False
+    )
+    updated_at = Column(
+        TIMESTAMP(timezone=True),
+        server_default= text('Now()'),
+        server_onupdate= text('Now()'),
+        nullable=False,
+    )
